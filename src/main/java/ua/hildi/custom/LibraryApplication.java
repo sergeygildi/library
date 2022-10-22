9b694d57b0045d1be59d6b26a78c2ee489273965
serhiihildi <sergeygildi@gmail.com>
2022-10-22 19:46:17 +0300

    feat: Project initialization

---
+++ src/main/java/ua/hildi/custom/LibraryApplication.java
@@ -0,0 +1,13 @@
+package ua.hildi.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/hildi/custom/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hildi.custom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
